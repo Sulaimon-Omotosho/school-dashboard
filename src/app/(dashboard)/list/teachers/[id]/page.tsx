d09dab5cd9b3ffcd9dee36ec276c1c57f8462303
Sulaimon Omotosho <susudef2u@gmail.com>
2025-01-29 22:18:19 +0100

    Chore: Bug fix on lists

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -134,7 +134,7 @@ const SingleTeacherPage = () => {
         {/* BOTTOM  */}
         <div className='mt-4 bg-white rounded-md p-4 h-[800px]'>
           <h1 className=''>Teacher&apos;s Schedule</h1>
-          <BigCalendar />
+          {/* <BigCalendar /> */}
         </div>
       </div>
       {/* RIGHT  */}
